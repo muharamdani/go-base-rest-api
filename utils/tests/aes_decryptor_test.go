02371e69dfa128c78ccdae959bfd1a352b34f1d5
Muhamad Ramdani <mramdani551@gmail.com>
2022-08-23 10:29:44 +0700

    Update remove whitespace

--- utils/tests/aes_decryptor_test.go
+++ utils/tests/aes_decryptor_test.go
@@ -8,10 +8,18 @@ import (
 )
 import "github.com/muharamdani/go-base-rest-api/utils"
 
-func TestAESDecrypt(t *testing.T) {
+func TestEqualAESDecrypt(t *testing.T) {
 	key := "12345678901234567890123456789012"
 	iv := "1234567890123456"
 	ct := "aKTs8BBg4ZL2oiM2Hy/oPg=="
 	res := utils.AESDecrypt(key, iv, ct)
 	assert.Equal(t, "Test", res)
+}
+
+func TestNotEqualAESDecrypt(t *testing.T) {
+	key := "asdfghjklqwertyuopsdfdghjklxcvbn"
+	iv := "djkalskdjkalqisj"
+	ct := "aKTs8BBg4ZL2oiM2Hy/oPg=="
+	res := utils.AESDecrypt(key, iv, ct)
+	assert.NotEqual(t, "Test", res)
 }
